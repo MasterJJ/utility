38e98ea14cfb7036c4836bcceccb600c9bf26243
masterj <ronchaos@gmail.com>
2017-04-28 21:17:19 +0900

    * add download font and install shellscript file

---
+++ vim/downloadFontandInstall.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "git clone https://github.com/powerline/fonts.git"
+git clone https://github.com/powerline/fonts.git
+
+echo "cd fonts"
+echo "install fonts ./install.sh"
+./install.sh
+
+exit 0
