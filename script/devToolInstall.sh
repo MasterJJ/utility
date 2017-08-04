c363f02aed5ffe5ebf8154da56e6070ca7004c41
masterj <ronchaos@gmail.com>
2017-08-05 00:26:22 +0900

    auto install develop tools

---
+++ script/devToolInstall.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "apt-get update and build tool install vim git subverson build-essential cgtags......"
+sudo apt-get -y update
+sudo apt-get -y install vim git subversion build-essential ctags
+
+
