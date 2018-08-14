b7cf097ed7409790b6cdaec39ef3b3c54a088c26
masterj <ronchaos@gmail.com>
2018-08-14 16:38:02 +0900

    update

--- script/devpkg_install_for_blockchain.sh
+++ script/devpkg_install_for_blockchain.sh
@@ -2,6 +2,6 @@
 sudo apt-get install build-essential libtool autotools-dev autoconf pkg-config libssl-dev
 sudo apt-get install libboost-all-dev
 sudo apt-get install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler
-sudo apt-get install libqrencode-dev autoconf openssl libssl-dev libevent-dev
+sudo apt-get install libqrencode-dev autoconf openssl libssl-dev libevent-dev libdb++-dev
 sudo apt-get install libminiupnpc-dev
 
