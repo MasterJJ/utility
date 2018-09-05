67ca53d4cbeb47b6fbbb82e0c2310fdea4538b3d
masterjj <ronchaos@gmail.com>
2018-09-05 11:00:53 +0900

    * update devpkg for freeradius on ubuntu system

---
+++ script/devpkg_install_for_freeradius.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt-get install build-essential libtool autotools-dev autoconf pkg-config libssl-dev
+sudo apt-get install libboost-all-dev
+sudo apt-get install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler
+sudo apt-get install libqrencode-dev autoconf openssl libssl-dev libevent-dev libdb++-dev
+sudo apt-get install libminiupnpc-dev
+sudo apt-get install libtalloc-dev 
+sudo apt-get install libkqueue-dev 
+sudo apt-get install llibssl-dev
+sudo apt-get install libhiredis-dev 
