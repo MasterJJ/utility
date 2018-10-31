bba5878e77fe723350c00caae1f80ff36cb3ecd1
masterj <ronchaos@gmail.com>
2018-10-31 21:11:24 +0900

    * update devpkg script

--- script/devpkg_install_for_blockchain.sh
+++ script/devpkg_install_for_blockchain.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# bitcoin
 sudo apt-get install build-essential libtool autotools-dev autoconf pkg-config libssl-dev
 sudo apt-get install libboost-all-dev
 sudo apt-get install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler
