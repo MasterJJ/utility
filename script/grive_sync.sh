b4606f9d07a34c2c116aa821a35f8b8fb1827216
masterj <ronchaos@gmail.com>
2018-12-11 23:35:22 +0900

    add grive_sync.sh

---
+++ script/grive_sync.sh
@@ -0,0 +1,16 @@
+#/usr/local/bin/grive --path /root/httpRoot/googledrv
+#sleep 10
+
+#/usr/local/bin/grive --path /root/httpRoot/googledrv
+#sleep 10
+
+#/usr/local/bin/grive --path /root/httpRoot/googledrv
+#sleep 10
+
+#/usr/local/bin/grive --path /root/httpRoot/googledrv
+#sleep 10
+
+#/usr/local/bin/grive --path /root/httpRoot/googledrv
+#sleep 10
+
+/usr/local/bin/grive --path /root/httpRoot/googledrv
