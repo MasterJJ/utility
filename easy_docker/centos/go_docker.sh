46a82a40c4d1cf26ed3e259da667b08db6b0a244
masterj <ronchaos@gmail.com>
2018-12-12 09:14:03 +0900

    update

---
+++ easy_docker/centos/go_docker.sh
@@ -0,0 +1,4 @@
+## sudo docker run -i -t --name my1 ubuntu /bin/bash
+sudo docker start mycentos
+sudo docker exec -it mycentos bash
+
---
+++ easy_docker/centos/make_docker.sh
@@ -0,0 +1,4 @@
+sudo docker run -i -t --name mycentos centos /bin/bash
+## sudo docker start my1
+## sudo docker attach my1
+
