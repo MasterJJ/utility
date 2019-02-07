cb79a20c8b108501416ce6382a8e734570cb65c5
masterj <ronchaos@gmail.com>
2019-02-07 15:27:48 +0000

    update raspi_opencv

---
+++ script/devToolInstall_raspi_opencv.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sudo apt-get update  -y
+##sudo apt-get upgrade -y
+
+sudo apt-get install -y build-essential cmake unzip pkg-config
+
+sudo apt-get install -y libjpeg-dev libpng-dev libtiff-dev
+sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+sudo apt-get install -y libxvidcore-dev libx264-dev
+sudo apt-get install -y libgtk-3-dev
+sudo apt-get install -y libcanberra-gtk*
+
+sudo apt-get install -y libatlas-base-dev gfortran
+
+sudo apt-get install -y python3-dev
+
---
+++ script/devToolInstall_raspi_opencv_core.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+wget -O opencv.zip https://github.com/Itseez/opencv/archive/3.3.0.zip
+unzip opencv.zip
+wget -O opencv_contrib.zip https://github.com/Itseez/opencv_contrib/archive/3.3.0
+unzip opencv_contrib.zip
+
