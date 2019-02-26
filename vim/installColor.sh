ccc425009c6f15e710071a0eb118456e1bf16bd8
masterj <ronchaos@gmail.com>
2019-02-26 10:58:22 +0900

    * update vim version vim80

--- vim/installColor.sh
+++ vim/installColor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "cp ./color/darkblack.vim /usr/share/vim/vim74/colors/."
-sudo cp ./color/darkblack.vim /usr/share/vim/vim74/colors/.
+echo "cp ./color/darkblack.vim /usr/share/vim/vim80/colors/."
+sudo cp ./color/darkblack.vim /usr/share/vim/vim80/colors/.
 
 exit 0
 
