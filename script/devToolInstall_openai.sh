e7adada57d874266fac702765e4067c9d5c8382b
masterj <ronchaos@gmail.com>
2019-04-10 14:28:46 +0900

    *update

--- script/devToolInstall_openai.sh
+++ script/devToolInstall_openai.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo apt-get -y update
 sudo apt-get install python-pip
-sudo apt-get install -y python-numpy python-dev cmake zlib1g-dev libjpeg-dev xvfb libav-tools xorg-dev python-opengl libboost-all-dev libsdl2-dev swig
+#sudo apt-get install -y python-numpy python-dev cmake zlib1g-dev libjpeg-dev xvfb libav-tools xorg-dev python-opengl libboost-all-dev libsdl2-dev swig
+sudo apt-get install -y python-numpy python-dev cmake zlib1g-dev libjpeg-dev xvfb xorg-dev python-opengl libboost-all-dev libsdl2-dev swig
