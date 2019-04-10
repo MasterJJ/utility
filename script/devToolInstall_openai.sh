50ee5f6f2c7980ea2d28e3a6804954b36302bbb9
masterj <ronchaos@gmail.com>
2019-04-10 14:25:01 +0900

    *add script

---
+++ script/devToolInstall_openai.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo apt-get -y update
+sudo apt-get install python-pip
+sudo apt-get install -y python-numpy python-dev cmake zlib1g-dev libjpeg-dev xvfb libav-tools xorg-dev python-opengl libboost-all-dev libsdl2-dev swig
