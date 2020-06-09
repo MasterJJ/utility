72931a8e3653ecf1316a040c8e3217691b518abe
masterj <ronchaos@gmail.com>
2020-06-09 09:28:12 +0900

    *update docker install add user docker group

--- script/dockerInstall.sh
+++ script/dockerInstall.sh
@@ -6,4 +6,6 @@ sudo apt-get -y install curl
 echo " install docker !!!"
 curl -s https://get.docker.com/ | sudo sh
 
-
+u="$USER"
+echo " auto enroll docker group add $u"
+sudo usermod -aG docker $u
