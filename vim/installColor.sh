70a8d4f41c62ff151ecc8c70cac4e4d926a6c5ba
masterj <ronchaos@gmail.com>
2021-02-05 07:28:47 +0900

    * update vim color path   vim80 -> vim81

--- vim/installColor.sh
+++ vim/installColor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "cp ./color/darkblack.vim /usr/share/vim/vim80/colors/."
-sudo cp ./color/darkblack.vim /usr/share/vim/vim80/colors/.
+echo "cp ./color/darkblack.vim /usr/share/vim/vim81/colors/."
+sudo cp ./color/darkblack.vim /usr/share/vim/vim81/colors/.
 
 exit 0
 
