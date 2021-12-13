84ed1be8b1072f14aac2147408a02d79bf1c8f51
masterj <ronchaos@gmail.com>
2021-12-13 16:36:24 +0900

    * update easy install docker script

--- easy_docker/install_docker_script.sh
+++ easy_docker/install_docker_script.sh
@@ -2,3 +2,14 @@ echo " install current docker easy script to ~/docker/ "
 
 mkdir ~/docker
 cp *  ~/docker/.
+
+
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+apt-cache policy docker-ce
+
+
+sudo apt install docker-ce
