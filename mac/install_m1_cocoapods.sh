159039bb7775d0b85a6bc8106e206388c35b8172
jmincha <jmincha@crossent.com>
2022-01-20 17:57:45 +0900

    * add file m1 cpu install error cocoapods, support script

---
+++ mac/install_m1_cocoapods.sh
@@ -0,0 +1,10 @@
+## change terminal app option,  enable rogetta2 
+## all  mission succes, after change terminal app option enable rogetta2
+
+sudo gem install cocoapods
+sudo gem isntall ffi
+
+## try pod install
+
+echo "try pod install"
+
