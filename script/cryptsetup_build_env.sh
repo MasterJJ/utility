563f9785847dda4d2f4971238e0ce7979d593c07
masterjj <ronchaos@gmail.com>
2022-06-23 15:00:44 +0900

    add scryptset build env

---
+++ script/cryptsetup_build_env.sh
@@ -0,0 +1,1 @@
+sudo apt-get install git gcc make autoconf automake autopoint pkg-config libtool gettext libssl-dev libdevmapper-dev libpopt-dev uuid-dev libsepol1-dev libjson-c-dev libssh-dev libblkid-dev tar
