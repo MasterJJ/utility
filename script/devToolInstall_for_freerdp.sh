c1533e10b90cf49176c800fefd51d4cfae95da48
masterj <ronchaos@gmail.com>
2022-07-14 10:55:33 +0900

    * add devtoolInstall for freerdp   env os ubuntu 18.04 lts   build dependency

---
+++ script/devToolInstall_for_freerdp.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# freerdp,  OS ubuntu 18.04 LTS
+sudo apt install build-essential git-core cmake libssl-dev libx11-dev libxext-dev libxinerama-dev \
+  libxcursor-dev libxdamage-dev libxv-dev libxkbfile-dev libasound2-dev libcups2-dev libxml2 libxml2-dev \
+  libxrandr-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev \
+  libxi-dev libavutil-dev \
+  libavcodec-dev libxtst-dev libgtk-3-dev libgcrypt20-dev libssh-dev libpulse-dev \
+  libvte-2.91-dev libxkbfile-dev libtelepathy-glib-dev libjpeg-dev \
+  libgnutls28-dev libavahi-ui-gtk3-dev libvncserver-dev \
+  libappindicator3-dev intltool libsecret-1-dev libwebkit2gtk-4.0-dev libsystemd-dev \
+  libsoup2.4-dev libjson-glib-dev libavresample-dev libsodium-dev \
+  libusb-1.0-0-dev libpcre2-dev libicu-dev
+
